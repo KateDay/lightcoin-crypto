d450dd0ae2bf8e40bca64b9ac98ae254c42ec457
Kate <khummle@gmail.com>
2019-05-29 15:06:11 +0000

    driver refactored after setting a getter

--- index.js
+++ index.js
@@ -5,26 +5,25 @@ class Transaction {
     this.amount = amount;
     this.account = account
   }
+  commit() {
+    this.account.balance += this.value;
+  }
 }
 
 class Withdrawal extends Transaction {
-
-  commit() {
-    this.account.balance -= this.amount;
+  get value() {
+    return -this.amount;
   }
-
 }
 
 class Deposit extends Transaction {
-
-  commit() {
-    this.account.balance += this.amount;
+  get value() {
+    return this.amount;
   }
 
 }
 
 class Account {
-
   constructor(username) {
     this.username = username;
     this.balance = 0;
@@ -36,19 +35,33 @@ const myAccount = new Account("snow-patrol");
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
+// console.log('Starting balance:', myAccount.balance);
 
-t1 = new Withdrawal(50.25, myAccount);
+// t1 = new Withdrawal(50.25, myAccount);
+// t1.commit();
+// console.log('Transaction 1:', t1);
+
+// t2 = new Withdrawal(9.99, myAccount);
+// t2.commit();
+// console.log('Transaction 2:', t2);
+
+// t3 = new Deposit(120.00, myAccount);
+// t3.commit();
+// console.log('Transaction 3:', t3);
+
+// console.log('Final Balance:', myAccount.balance);
+
+
+const billBo = new Account('billybob');
+console.log('starting Balance:', billBo.balance);
+
+const t1 = new Deposit(120.00, billBo);
 t1.commit();
-console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99, myAccount);
+const t2 = new Withdrawal(50.00, billBo);
 t2.commit();
-console.log('Transaction 2:', t2);
 
-t3 = new Deposit(120.00, myAccount);
-t3.commit();
-console.log('Transaction 3:', t3);
+console.log('Final Balance:', billBo.balance);
 
-console.log('Balance:', myAccount.balance);
 
 
